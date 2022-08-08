8a71bee7c034cac554a67399bcd69ed319e27502
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-08 19:38:42 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const htmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, "src", "index.js"),
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "bundler[hash].js",
+		clean: true
+	},
+
+	plugins: [
+		new htmlWebPackPlugin({
+			template: path.resolve(__dirname, "public", "index.html"),
+		})
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	},
+
+	devServer: {
+		port: 3000,
+	}
+
+}
