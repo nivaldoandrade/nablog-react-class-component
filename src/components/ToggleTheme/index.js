ad521bc06cf9093459f75326183f74c52319f219
nivaldoandrade <nivaldoandradef@gmail.com>
2022-08-14 15:05:58 -0300

    consuming the Context API

--- src/components/ToggleTheme/index.js
+++ src/components/ToggleTheme/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { ThemeContext } from '../../contexts/ThemeContext';
+
+
+import { Button } from './styles';
+
 export default class ToggleTheme extends React.Component {
 	render () {
 		return (
-			<>
-				🌚
-			</>
+			<ThemeContext.Consumer>
+				{({theme, handleChangeToggleTheme}) => (
+					<Button
+						onClick={handleChangeToggleTheme}
+					>
+						{theme === 'dark' ? '🌚' : '🌝'}
+					</Button>
+				)}
+			</ThemeContext.Consumer>
 		)
 	}
 }
---
+++ src/components/ToggleTheme/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+	font-size: 24px;
+	background-color: transparent;
+	border: 0;
+	padding: 2px;
+	cursor: pointer;
+`;
