0d0fcd5134fb7cca60a896ac926574bffdd71da2
nivaldoandrade <nivaldoandradef@gmail.com>
2022-09-21 10:23:35 -0300

    componentDidUpdate()

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import { Container, Title, Icon } from './styles';
 
 //useEffect(() => {}) é executado a cada renderização, e para temos o mesmo efeito em class component é só colocar a function dentro do render().
 //useEffect(() => {}, []) é executado na primeira renderização, e para temos o mesmo efeito em class componet é só utilizar o componentDidMount().
+//componentDidUpdate é chamado depois do render() e possui 2 parametros(prevProps e prevState) que são os valores anteriores das props e states.
 
 export default class Header extends React.Component {
 	state = {
@@ -13,12 +14,20 @@ export default class Header extends React.Component {
 	}
 
 	handleChangeState = () => {
-		this.setState({title: 'NaBlogChange'});
+		this.setState({title: 'NaBlogChange', TesteDoTeste: false});
 	};
 
 	componentDidMount() {
 		console.log('componentDidMount executed');
 	};
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log({
+			currentState: this.state,
+			prevProps,
+			prevState
+		})
+	}
 
 	render() {
 		const {title} = this.state;
